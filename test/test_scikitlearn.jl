209aa78dedb2523013be6f6411789be6d6450407
Paulito Palmes <ppalmes@gmail.com>
2019-02-04 16:51:34 +0000

    unit-testing for scikitlearn regression

--- test/test_scikitlearn.jl
+++ test/test_scikitlearn.jl
@@ -14,32 +14,67 @@ const IRIS = dataset("datasets","iris")
 const X = IRIS[:,1:4] |> Matrix
 const Y = IRIS[:,5] |> Vector
 
+const XX = IRIS[:,1:1] |> Matrix
+const YY = IRIS[:,4] |> Vector
+
 const classifiers = [
-	"LinearSVC","QDA","MLPClassifier","BernoulliNB",
-	"RandomForestClassifier","LDA",
-  	"NearestCentroid","SVC","LinearSVC","NuSVC","MLPClassifier",
-  	"RidgeClassifierCV","SGDClassifier","KNeighborsClassifier",
-  	"GaussianProcessClassifier","DecisionTreeClassifier",
+    "LinearSVC","QDA","MLPClassifier","BernoulliNB",
+    "RandomForestClassifier","LDA",
+    "NearestCentroid","SVC","LinearSVC","NuSVC","MLPClassifier",
+    "RidgeClassifierCV","SGDClassifier","KNeighborsClassifier",
+    "GaussianProcessClassifier","DecisionTreeClassifier",
     "PassiveAggressiveClassifier","RidgeClassifier",
-	"ExtraTreesClassifier","GradientBoostingClassifier",
-	"BaggingClassifier","AdaBoostClassifier"
+    "ExtraTreesClassifier","GradientBoostingClassifier",
+    "BaggingClassifier","AdaBoostClassifier","GaussianNB","MultinomialNB",
+    "ComplementNB","BernoulliNB"
  ]
 
-function fit_test(learner::String)
+const regressors = [
+    "SVR",
+    "Ridge",
+    "RidgeCV",
+    "Lasso",
+    "ElasticNet",
+    "Lars",
+    "LassoLars",
+    "OrthogonalMatchingPursuit",
+    "BayesianRidge",
+    "ARDRegression",
+    "SGDRegressor",
+    "PassiveAggressiveRegressor",
+    "KernelRidge",
+    "KNeighborsRegressor",
+    "RadiusNeighborsRegressor",
+    "GaussianProcessRegressor",
+    "DecisionTreeRegressor",
+    "RandomForestRegressor",
+    "ExtraTreesRegressor",
+    "GradientBoostingRegressor",
+    "MLPRegressor",
+    "AdaBoostRegressor"
+#    "IsotonicRegression"
+]
+    	
+
+function fit_test(learner::String,in::T,out::Vector) where {T<:Union{Matrix,Vector}}
 	_learner=SKLearner(Dict(:learner=>learner))
-	fit!(_learner,X,Y)
+	fit!(_learner,in,out)
 	println(learner)
 	@test _learner.model != nothing
 end
 
-function loopover()
-	for cls in classifiers
-		fit_test(cls)
-	end
+@testset "scikit classifiers" begin
+    for cls in classifiers
+	fit_test(cls,X,Y)
+    end
 end
 
-@testset "scikit leaners" begin
-	loopover()
+@testset "scikit regressors" begin
+    for rg in regressors
+	fit_test(rg,XX,YY)
+    end
 end
+
+
 
 end
