8cf5fdd07527b33567d0033bc992598e3a226456
Paulito Palmes <ppalmes@gmail.com>
2019-05-01 14:23:23 +0100

    adding cliwrapper for passing args in commandline

---
+++ src/cliwrapper.jl
@@ -0,0 +1,121 @@
+module CLIWrappers
+
+using TSML
+using TSML.TSMLTypes
+using TSML.TSMLTransformers
+using TSML.Utils
+
+using TSML.DataReaders
+using TSML.DataWriters
+using TSML.Statifiers
+using TSML.Monotonicers
+
+using Dates
+using DataFrames
+using CSV
+
+export tsmlrun
+
+const COMMONARG = Dict(:dateformat=>"dd/mm/yyyy HH:MM",
+                        :dateinterval=>Dates.Hour(1),
+                        :processmissing=>true
+                       )
+
+function tsmlrun(inputname::AbstractString,outputname::AbstractString="",datefmt::AbstractString="dd/mm/yyyy HH:MM",otype::AbstractString="table")
+    if otype == "table"
+        imputedoutput(inputname,outputname,datefmt)
+    elseif otype == "stat"
+        imputedstat(inputname,outputname,datefmt)
+    else
+        error("wrong output type")
+    end
+end
+
+function rawstat(inputname::AbstractString,outputname::AbstractString="",datefmt::AbstractString="dd/mm/yyyy HH:MM")
+    isfile(inputname) || error("input file name does not exist")
+    csvreader = DataReader(Dict(:filename=>inputname,:dateformat=>datefmt))
+    stfier = Statifier()
+    lpipe = Pipeline(Dict(
+        :transformers => [csvreader,stfier]
+      )
+    )
+    fit!(lpipe)
+    res=transform!(lpipe)
+    if outputname != ""
+        res |> CSV.write(outputname)
+    end
+    return res
+end
+
+function aggregatedstat(inputname::AbstractString,outputname::AbstractString="",datefmt::AbstractString="dd/mm/yyyy HH:MM")
+    isfile(inputname) || error("input file name does not exist")
+    csvreader = DataReader(Dict(:filename=>inputname,:dateformat=>datefmt))
+    valgator = DateValgator(COMMONARG)
+    stfier = Statifier()
+    lpipe = Pipeline(Dict(
+        :transformers => [csvreader,valgator,stfier]
+      )
+    )
+    fit!(lpipe)
+    res=transform!(lpipe)
+    if outputname != ""
+        res |> CSV.write(outputname)
+    end
+    return res
+end
+
+function aggregatedoutput(inputname::AbstractString,outputname::AbstractString="",datefmt::AbstractString="dd/mm/yyyy HH:MM")
+    isfile(inputname) || error("input file name does not exist")
+    csvreader = DataReader(Dict(:filename=>inputname,:dateformat=>datefmt))
+    valgator = DateValgator(COMMONARG)
+    lpipe = Pipeline(Dict(
+        :transformers => [csvreader,valgator]
+      )
+    )
+    fit!(lpipe)
+    res=transform!(lpipe)
+    if outputname != ""
+        res |> CSV.write(outputname)
+    end
+    return res
+end
+
+function imputedstat(inputname::AbstractString,outputname::AbstractString="",datefmt::AbstractString="dd/mm/yyyy HH:MM")
+    isfile(inputname) || error("input file name does not exist")
+    csvreader = DataReader(Dict(:filename=>inputname,:dateformat=>datefmt))
+    valgator = DateValgator(COMMONARG)
+    valnner = DateValNNer(COMMONARG)
+    mononicer = Monotonicer()
+    stfier = Statifier()
+    lpipe = Pipeline(Dict(
+        :transformers => [csvreader,valgator,valnner,mononicer,stfier]
+      )
+    )
+    fit!(lpipe)
+    res=transform!(lpipe)
+    if outputname != ""
+        res |> CSV.write(outputname)
+    end
+    return res
+end
+
+function imputedoutput(inputname::AbstractString,outputname::AbstractString="",datefmt::AbstractString="dd/mm/yyyy HH:MM")
+    isfile(inputname) || error("input file name does not exist")
+    csvreader = DataReader(Dict(:filename=>inputname,:dateformat=>datefmt))
+    valgator = DateValgator(COMMONARG)
+    valnner = DateValNNer(COMMONARG)
+    mononicer = Monotonicer()
+    stfier = Statifier()
+    lpipe = Pipeline(Dict(
+        :transformers => [csvreader,valgator,valnner,mononicer]
+      )
+    )
+    fit!(lpipe)
+    res=transform!(lpipe)
+    if outputname != ""
+        res |> CSV.write(outputname)
+    end
+    return res
+end
+
+end
---
+++ test/test_cliwrapper.jl
@@ -0,0 +1,25 @@
+module TestCLIWrapper
+
+using TSML
+using Statistics
+using Test
+
+using TSML.CLIWrappers:tsmlrun,rawstat,aggregatedstat,aggregatedoutput,imputedstat,imputedoutput
+
+const inputname = joinpath(dirname(pathof(TSML)),"../data/testdata.csv")
+const outputname = joinpath(dirname(pathof(TSML)),"../data/testdata_output.csv")
+
+function test_cli()
+  op1=tsmlrun(inputname,"")
+  op2=imputedoutput(inputname,"")
+  @test mean(op1[:Value] .== op2[:Value]) == 1.0
+  op5=tsmlrun(inputname,"","dd/mm/yyyy HH:MM","stat")
+  op6=imputedstat(inputname,"")
+  @test sum(op5[1,:] .== op6[1,:]) == 20
+end
+@testset "CLIWrapper: check cliwrapper" begin
+  test_cli()
+end
+
+
+end
