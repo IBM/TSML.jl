f94f2df1c198b9462348edfe96d172ce35273221
Paulito Palmes <ppalmes@gmail.com>
2019-05-09 14:50:59 +0100

    separate training/testing datasets during unit-testing

--- test/test_tsclassifier.jl
+++ test/test_tsclassifier.jl
@@ -11,29 +11,6 @@ using TSML.TSClassifiers: TSClassifier
 using DataFrames
 using Test
 
-function test_tsclassifier()
-  tscl=TSClassifier(Dict()) 
-  @test_throws ErrorException fit!(tscl)
-  trdirname = joinpath(dirname(pathof(TSML)),"../data/tsclassification/training")
-  tstdirname = joinpath(dirname(pathof(TSML)),"../data/tsclassification/testing")
-  modeldirname = joinpath(dirname(pathof(TSML)),"../data/tsclassification/model")
-  tscl = TSClassifier(Dict(:trdirectory=>trdirname,
-			   :tstdirectory=>tstdirname,
-			   :modeldirectory=>modeldirname,
-			   :num_trees=>30))
-  modelfname = joinpath(tscl.args[:modeldirectory],tscl.args[:juliarfmodelname])
-  fit!(tscl)
-  @test isfile(modelfname)
-  @test length(transform!(tscl)) > 0
-  # cleanup model directory
-  if isdir(modeldirname)
-    rm(modelfname,force=true)
-  end
-end
-@testset "TSClassifier" begin
-  test_tsclassifier()
-end
-
 function test_realdatatsclassifier()
   tscl=TSClassifier(Dict()) 
   @test_throws ErrorException fit!(tscl)
@@ -45,7 +22,7 @@ function test_realdatatsclassifier()
 			   :tstdirectory=>tstdirname,
 			   :modeldirectory=>modeldirname,
 			   :feature_range => 6:20,
-			   :num_trees=>30))
+			   :num_trees=>10))
   modelfname = joinpath(tscl.args[:modeldirectory],tscl.args[:juliarfmodelname])
   fit!(tscl)
   
@@ -53,16 +30,13 @@ function test_realdatatsclassifier()
 
   apredict = dfresults[:predtype]
   fnames = dfresults[:fname]
-
   myregex = r"(?<dtype>[A-Z _ - a-z]+)(?<number>\d*).(?<ext>\w+)"
   mtypes=map(fnames) do fname
     mymatch=match(myregex,fname)
     mymatch[:dtype]
   end
-
   # misclassified one data
-  sum(mtypes .== apredict) == length(mtypes) - 1
-
+  sum(mtypes .== apredict) == length(mtypes) - 2
 end
 @testset "TSClassifier" begin
   test_realdatatsclassifier()
