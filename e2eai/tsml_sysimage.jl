e7cc3591f74dbd02b71a59a135e7437c0b04463b
Paulito Palmes, PhD <ppalmes@gmail.com>
2024-05-24 19:43:59 +0100

    refactor
    Signed-off-by: Paulito Palmes, PhD <ppalmes@gmail.com>

--- e2eai/tsml_sysimage.jl
+++ e2eai/tsml_sysimage.jl
@@ -2,7 +2,7 @@ using PackageCompiler
 create_sysimage(["AMLPipelineBase","DataFrames","StatsBase",
                   "ArgParse","TSML","CSV","Dates","Distributed",
                   "Random","ArgParse","Test","Distributed",
-                  "Statistics","Serialization","StatsBase","Test"]; 
+                  "Statistics","Serialization","Test"]; 
                   sysimage_path="tsml.so",
                   include_transitive_dependencies=false,
                   precompile_execution_file="tsml_precompile.jl")
